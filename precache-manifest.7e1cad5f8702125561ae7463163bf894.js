d26fa021d3a03e63b22436056feb927a42204351
jakermate <jmcfly@gmail.com>
2020-02-27 13:36:43 -0800

    Updates

---
+++ precache-manifest.7e1cad5f8702125561ae7463163bf894.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0485dd059dcde6df8f9f75bc8d2c9e",
+    "url": "/jakethesnake/index.html"
+  },
+  {
+    "revision": "94ca82c75533491bd82b",
+    "url": "/jakethesnake/static/css/main.20a8ce4e.chunk.css"
+  },
+  {
+    "revision": "900b03247611d5b0cfe0",
+    "url": "/jakethesnake/static/js/2.2a8ae0ed.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/jakethesnake/static/js/2.2a8ae0ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ca82c75533491bd82b",
+    "url": "/jakethesnake/static/js/main.b0ad8473.chunk.js"
+  },
+  {
+    "revision": "d520e413099b2460a318",
+    "url": "/jakethesnake/static/js/runtime-main.1a38b9c9.js"
+  },
+  {
+    "revision": "7d360a753f2d38463321245f5aa0d37d",
+    "url": "/jakethesnake/static/media/arrow-active.7d360a75.svg"
+  },
+  {
+    "revision": "1aab54ce82c5eddd16a1c24dd32c6985",
+    "url": "/jakethesnake/static/media/arrow-disabled.1aab54ce.svg"
+  },
+  {
+    "revision": "cd0060780823c166a7e4a055ffaab2f1",
+    "url": "/jakethesnake/static/media/arrow-select.cd006078.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jakethesnake/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68c161616ef079046006dd76d38fc7eb",
+    "url": "/jakethesnake/static/media/rat.68c16161.svg"
+  }
+]);
